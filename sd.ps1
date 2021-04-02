ebbf9f0465f4e167948cf99fbc134cfd6e0e013b
Felipe Leite <lipi4shared@hotmail.com>
2021-04-02 09:45:28 -0300

    Adicionando script para programar ou cancelar desligamento automático do WIndows.

---
+++ sd.ps1
@@ -0,0 +1,20 @@
+﻿switch ( $args[0] ) {
+   "--help" { 
+        "Shutdown helper:";
+        "<number> Tempo em segundos para desligamento.";
+        "cancel   Cancelar desligamento agendado.";
+        break 
+   }
+
+   "cancel" { 
+        "Cancelando desligamento...";
+        shutdown -a;
+        "Cancelado!";
+        break 
+   }
+
+   default { 
+        shutdown -s -t $args
+        break
+   }
+}
