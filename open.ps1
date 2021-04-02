3f8120b80754d2ab86da9b3fe9f3738115ccc24c
Felipe Leite <lipi4shared@hotmail.com>
2021-04-02 09:26:17 -0300

    Adicionando scripts open e run.

---
+++ open.ps1
@@ -0,0 +1,26 @@
+﻿# Script para abrir projetos Node no VSCode
+
+switch ( $args[0] ) {
+   # É possível ter projetos predefinidos:
+   "app_1"{ 
+        code C:\path-to-app-1;
+        break
+   }
+   "app_2"{ 
+        code C:\path-to-app-2;
+        break
+   }
+
+   # É abrir uma lista de executáveis:
+   "executable_group_1"{
+        C:\Users\path-to-exe-file\MyApp1.exe
+        # Alguns paths exigem aspas:
+        & "C:\Users\path-to-exe-file\MyApp2.exe"
+   }
+
+   # Como default é possivel definir um comportamento padrão usando o argumento passado:
+   default{ 
+        code C:\path-to-projects-folder\$args;
+        break
+   }
+}
---
+++ run.ps1
@@ -0,0 +1,9 @@
+﻿# Script para executar aplicações node com o Yarn.
+
+switch ( $args[0] ) {
+   # É possível ter projetos predefinidos:
+   "app_1" { cd C:\path-to-node-project; cls; yarn dev;   break }
+
+   # Como default é possivel definir um comportamento padrão usando o argumento passado:
+   default { cd C:\MyProjects\$args;     cls; yarn start; break }
+}
